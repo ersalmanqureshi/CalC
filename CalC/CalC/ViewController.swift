0fe1ada6b19a7d5d0f7a0ec0ec0cbcae83991be5
Salman Qureshi <er.salmanqureshi@gmail.com>
2017-07-04 20:56:55 +0530

    Empty project created

---
+++ CalC/CalC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalC
+//
+//  Created by Salman Qureshi on 04/07/17.
+//  Copyright © 2017 iSalmanLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
