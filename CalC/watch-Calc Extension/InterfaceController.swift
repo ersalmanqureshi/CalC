5af0f31ff467e5a90c1f3467868a5a1c70cdba95
Salman Qureshi <er.salmanqureshi@gmail.com>
2017-07-20 10:02:45 +0530

    Updated design and connected action to button in watch app

--- CalC/watch-Calc Extension/InterfaceController.swift
+++ CalC/watch-Calc Extension/InterfaceController.swift
@@ -12,6 +12,73 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    
+    @IBAction func oneTap() {
+        print("1")
+    }
+    
+    @IBAction func twoTap() {
+        print("2")
+    }
+    
+    @IBAction func threeTap() {
+        print("3")
+    }
+    
+    @IBAction func fourTap() {
+        print("4")
+    }
+    
+    @IBAction func fiveTap() {
+        print("5")
+    }
+    
+    @IBAction func sixTap() {
+        print("6")
+    }
+    
+    @IBAction func sevenTap() {
+        print("7")
+    }
+    
+    @IBAction func eightTap() {
+        print("8")
+    }
+    
+    @IBAction func nineTap() {
+        print("9")
+    }
+    
+    @IBAction func zeroTap() {
+        print("0")
+    }
+    
+    
+    @IBAction func multiplyOperation() {
+        print("*")
+    }
+    
+    @IBAction func addOperation() {
+        print("+")
+    }
+    
+    @IBAction func subtractOperation() {
+        print("-")
+    }
+    
+    @IBAction func divideOperation() {
+        print("divide")
+    }
+    
+    @IBAction func decimalOperation() {
+        print(".")
+    }
+    
+    @IBAction func resultTap() {
+        print("=")
+    }
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
